5666ae731a73befe7b048bf2af6fc27c5930db71
twei2001 <tweish2001@gmail.com>
2023-12-27 15:24:39 -0500

    began js

---
+++ script.js
@@ -0,0 +1,8 @@
+const searchBtn = document.querySelector('.btn')
+const searchBar = document.querySelector('.search-bar')
+const wordResult = document.querySelector('.word')
+const soundBtn = document.querySelector('.volume')
+
+soundBtn.addEventListener("click", ()=>{
+console.log('hello')
+})
