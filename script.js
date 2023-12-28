2a4b0ffa499c763fb742bfb72817143bf3d972c8
twei2001 <tweish2001@gmail.com>
2023-12-28 10:20:29 -0500

    small changes

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+!(async function () {
+  const searchBar = document.querySelector(".search-bar");
+  const searchBtn = document.querySelector(".button");
+  const soundBtn = document.querySelector(".volume");
+  const wordResult = document.querySelector(".word");
+  const link = "https://api.dictionaryapi.dev/api/v2/entries/en/<word>";
 
-
-    const searchBtn = document.getElementById('sbutton');
-    const searchBar = document.querySelector('.search-bar')
-    const wordResult = document.querySelector('.word')
-    const soundBtn = document.querySelector('.volume')
-
-    searchBtn.addEventListener('click', ()=>{
-        console.log('hello')
-    })
+  searchBtn.addEventListener("click", () => {
+    console.log("btn working");
+  });
+  
+})();
