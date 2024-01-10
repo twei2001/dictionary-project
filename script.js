2ccb6f53558a7bc0023e2f1a851a5503e5cff748
twei2001 <tweish2001@gmail.com>
2024-01-09 21:51:38 -0500

    Cleaning up with minor adjustments

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
-!(async function () {
+( () => {
   const searchBar = document.querySelector(".search-bar");
   const searchBtn = document.querySelector(".button");
-  const soundBtn = document.querySelector(".volume");
-  const wordResult = document.querySelector(".word");
-  const dictionary = document.querySelector('.hide')
- 
+  const dictionary = document.querySelector(".hide");
 
+  const dictionaryApi = async (word) => {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    ).then((res) => res.json());
+    return response[0];
+  };
 
+  const updatePage = async () => {
+    const info = await dictionaryApi(searchBar.value);
 
-  const link = "https://api.dictionaryapi.dev/api/v2/entries/en/<word>";
-
-
-  
-  const dictionaryApi = async (word) =>{
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then(res => res.json())
-    return response[0]
-  }
-  
-  const updatePage = async () =>{
-    const info = await dictionaryApi(searchBar.value)
-   
-  
-    let partOfSpeechArr = []
-    for (let i = 0; i<info.meanings.length-1; i++){
-      partOfSpeechArr.push(info.meanings[i].partOfSpeech)
+    let partOfSpeechArr = [];
+    for (let i = 0; i < info.meanings.length - 1; i++) {
+      partOfSpeechArr.push(info.meanings[i].partOfSpeech);
     }
-dictionary.innerHTML = ` 
+    dictionary.innerHTML = ` 
 <div class="word__container">
-  <h3>${info.word}</h3><button  class="material-symbols-outlined">
-  <span> <audio controls src="${info.phonetics[0].audio}"></audio></span>       
-  </button>
-</div>
-<div class="details">${info.phonetic || ''}</div>
-<div class="details"> <span>${partOfSpeechArr.map(e => e).join(', ')}</span> </div>
-<div class="definition">${info.meanings[0].definitions[0].definition}
-<br><div class="sentence">${info.meanings[0].definitions[0].example || ''}</div>
-`
-
-  }
-  
-searchBtn.addEventListener("click", updatePage);
-
-searchBar.addEventListener("keypress", function(event) {
+  <h3>${info.word}</h3>
+   <audio controls src="${info.phonetics[0].audio}"></audio>
  
-  if (event.key === "Enter") {
-    updatePage()
-  }
+</div>
+<div class="details">${info.phonetic || ""}</div>
+<div class="details"> <span>${partOfSpeechArr
+      .map((e) => e)
+      .join(", ")}</span> </div>
+<div class="definition">${info.meanings[0].definitions[0].definition}
+<br><div class="sentence">${info.meanings[0].definitions[0].example || ""}</div>
+`;
+  };
 
-})
+  searchBtn.addEventListener("click", updatePage);
 
+  searchBar.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      updatePage();
+    }
+  });
 })();
-
